793c1cfd0d4d6ec1498446794dd0430bc5b6d871
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-08 23:29:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array) # code an argument here
 end
 
 def long_planeteer_calls(array) # code an argument here
-  array.include?{ 
+  array.include?()
 end
 
 def find_the_cheese# code an argument here
