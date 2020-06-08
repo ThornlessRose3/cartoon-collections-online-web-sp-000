ff33ca9b541bfde14ab900ec5af7c520c73a5ea0
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-08 23:21:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   array.each_wuth_index { |dwarf, index|
   puts "#{index} #{dwarf}"
+  }
 end
 
 def summon_captain_planet# code an argument here
